87fbc490771d5b5139346d364c42da36919e15a5
Khanh Le <khanh.lnq@gmail.com>
2011-09-28 09:14:30 +0700

    Deployed 1st version

---
+++ src/com/khanhlnq/vietutilities/servlets/StripDiacriticsServlet.java
@@ -0,0 +1,94 @@
+/**
+ * 
+ */
+package com.khanhlnq.vietutilities.servlets;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Logger;
+
+import javax.servlet.http.*;
+
+import net.sourceforge.vietpad.VietUtilities;
+
+/**
+ * @author khanhlnq
+ * 
+ */
+public class StripDiacriticsServlet extends HttpServlet {
+	private static final Logger log = Logger
+			.getLogger(StripDiacriticsServlet.class.getName());
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+
+		String urlParam = req.getParameter("url");
+
+		StringBuffer respBuf = new StringBuffer();
+		String respContentType = "text/plain";
+
+		if (urlParam != null) {
+			try {
+				URL url = new URL(urlParam.trim());
+				HttpURLConnection connection = (HttpURLConnection) url
+						.openConnection();
+				connection.setRequestMethod("GET");
+
+				int respCode = connection.getResponseCode();
+
+				if (respCode == HttpURLConnection.HTTP_OK) {
+					respContentType = connection.getContentType();
+					BufferedReader reader = new BufferedReader(
+							new InputStreamReader(connection.getInputStream(),
+									"UTF-8"));
+					String line;
+					while ((line = reader.readLine()) != null) {
+						respBuf.append(
+								fixSmartQuotes(VietUtilities
+										.stripDiacritics(line.trim()))).append(
+								"\n");
+					}
+					reader.close();
+				} else {
+					log.info("HTTP response code: " + respCode);
+				}
+			} catch (MalformedURLException e) {
+				// ...
+			} catch (IOException e) {
+				// ...
+			}
+		}
+
+		resp.setContentType(respContentType);
+		resp.setCharacterEncoding("UTF-8");
+		resp.getWriter().println(respBuf.toString().trim());
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		doGet(req, resp);
+	}
+
+	// replace Word processor smart quotes and ' with ascii if flag set
+	// http://www.coderanch.com/t/443932/java/java/bad-characters-MS-Windows-only
+	String fixSmartQuotes(String s) {
+		if (s == null || s.length() == 0) {
+			return s;
+		}
+		s = s.replace((char) 145, (char) '\'');
+		s = s.replace((char) 8216, (char) '\''); // left single quote
+		s = s.replace((char) 146, (char) '\'');
+		s = s.replace((char) 8217, (char) '\''); // right single quote
+		s = s.replace((char) 147, (char) '\"');
+		s = s.replace((char) 148, (char) '\"');
+		s = s.replace((char) 8220, (char) '\"'); // left double
+		s = s.replace((char) 8221, (char) '\"'); // right double
+		s = s.replace((char) 8211, (char) '-');
+
+		return s.replace((char) 150, (char) '-');
+	}
+}
